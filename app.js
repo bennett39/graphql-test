a5f4ac692fc7cc1a3c6f15c69b9360cb9f2d1024
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-05-23 14:53:31 -0400

    Up & running - GraphQL Hello

---
+++ app.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let graphqlHTTP = require('express-graphql');
+let { buildSchema } = require('graphql');
+
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+let root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+let app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => {console.log('Listening on port 4000.')});
