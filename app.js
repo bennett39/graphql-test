243b8df39e1c1d73e8e3ba6f2d980263fc277fa8
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-05-23 16:41:20 -0400

    Add rollDice query object

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ let { buildSchema } = require('graphql');
 let schema = buildSchema(`
   type Query {
     hello: String
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
 
 let root = {
   hello: () => {
     return 'Hello world!';
+  },
+  rollDice: ({numDice, numSides}) => {
+    let output = [];
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
   },
 };
 
